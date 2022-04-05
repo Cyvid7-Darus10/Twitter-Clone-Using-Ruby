59bad99cc4462eb240db0be6658183f7a545780b
Cyrus David Pastelero <cgpastelero@up.edu.ph>
2022-04-05 19:16:59 +0800

    fix(can't login): added the create method in before action

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, except: [:new, :edit]
+  before_action :logged_in_user, except: [:new, :edit, :create]
   before_action :correct_user, only: :edit
 
   def new
