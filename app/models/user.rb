83a4f9a0e7622187fefa4d55da8a07e6058af23e
Cyrus David Pastelero <cgpastelero@up.edu.ph>
2022-04-07 20:48:26 +0800

    chore: updated the model association for posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+    has_many :posts
     validates :name, presence: true, length: { maximum: 50 }
     EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
     validates :email, presence: true, length: { maximum: 50 }, format: { with: EMAIL_REGEX }, uniqueness: { case_sensitive: false }
